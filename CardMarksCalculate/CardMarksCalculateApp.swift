b1a20500eb9e932b4c3980a6624d001dbf925c6b
mo <kanguki2381@gmail.com>
2023-04-16 07:51:19 +0700

    Initial Commit

---
+++ CardMarksCalculate/CardMarksCalculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMarksCalculateApp.swift
+//  CardMarksCalculate
+//
+//  Created by Mo Nguyen on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardMarksCalculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
